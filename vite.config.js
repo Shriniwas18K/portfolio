271433a5924c3ca311f7a9ea187b786309222c16
sharqawycs <sharqawycs@gmail.com>
2025-02-16 07:46:08 +0200

    done

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,5 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react(), tailwindcss()],
-<<<<<<< HEAD
-    base: '/my-portfolio',
-=======
     base: '/portfolio/',
->>>>>>> 0e38046 (done)
 });
