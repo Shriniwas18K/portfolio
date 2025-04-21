fb54750c7272911017019326569c081cb5afbd54
Shriniwas Kulkarni <153389794+Shriniwas18K@users.noreply.github.com>
2025-04-21 19:10:13 +0530

    Update projectsData.js

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,43 +1,16 @@
 export const projectsData = [
     {
-        title: 'Recipe Finder',
+        title: 'SmartRentals-REST API',
         description:
-            'A web platform designed to make discovering and exploring recipes effortless, while helping users find recipes tailored to their weight and dietary preferences.',
-        skills: ['HTML', 'CSS', 'Plain JavaScript', 'API'],
-        link: 'https://github.com/SharQawyCS/recipe-finder',
+            'A backend REST API featuring CRUD operations for property postings and profile management',
+        skills: ['Spring Boot', 'Spring Data JPA', 'Spring Security', 'REST-API' ,'Maven' ,'Java'],
+        link: 'https://github.com/Shriniwas18K/Smartrentals-REST-API',
     },
     {
-        title: 'Optimal BST Data Structure',
+        title: 'Windows Monitoring',
         description:
-            'A C++ application for constructing and analyzing OBST Data Structure via a CLI. minimizes search costs using dynamic programming and provides interactive tools for tree creation, visualization, and analysis.',
-        skills: ['C++', 'Data Structure', 'Graphviz'],
-        link: 'https://github.com/SharQawyCS/Optimal-Binary-Search-Tree',
-    },
-    {
-        title: 'Predict the Item Price',
-        description:
-            'A Kaggle competition focused on predicting item prices based on historical data. Involved preprocessing, feature engineering, and implementing ML models to achieve the best predictions.',
-        skills: [
-            'Python',
-            'Pandas',
-            'NumPy',
-            'Scikit-learn',
-            'Regression Models',
-        ],
-        link: 'https://drive.google.com/file/d/1035xC_SnRYHKfXTMzbRrWeWCHrFGSW9q/view?usp=sharing',
-    },
-    {
-        title: 'To-Do List API',
-        description:
-            'A To-Do List API built with Express.js. The project was created as a foundational backend exercise to strengthen my understanding of REST architecture, authentication, and CRUD operations.',
-        skills: ['Node.js', 'Express.js', 'MongoDB', 'JWT', 'Postman'],
-        link: 'https://github.com/SharQawyCS/todo-list-api-expressjs',
-    },
-    {
-        title: 'Healthcare Management System',
-        description:
-            'A Java-based application designed for managing doctors, patients, and appointments in a healthcare environment. Built with Swing for the GUI and a text file for data storage.',
-        skills: ['Java', 'Swing', 'Text File Storage'],
-        link: 'https://github.com/SharQawyCS/healthcare-management-Java-system',
-    },
+            'An application that monitors physical CPU stats of machines in real time and ingest those into remote postgres database and visualize them into grafana for sending email alerts if usage exceeds beyond thresholds',
+        skills: ['Python', 'psutils', 'postgres' ,'grafana'],
+        link: 'https://github.com/Shriniwas18K/windows-monitoring',
+    }
 ];
